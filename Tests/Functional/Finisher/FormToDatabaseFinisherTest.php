448b065b209c4a2f1e15247e6edafa4916d2a6f9
mhofmann <m.hofmann@web-vision.de>
2024-11-08 00:26:54 +0100

    [TASK] Add FormToDatabaseFinisherTest
    This test is for checking a really simple form adding values correct to
    the database. As it was needed, this commit also introduces the
    SiteBasedTestTrait, which is helpful for frontend related tests and a
    simple test_extension as fixture for adding default frontend rendering
    to tests.

---
+++ Tests/Functional/Finisher/FormToDatabaseFinisherTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lavitto\FormToDatabase\Test\Functional\Finisher;
+
+use Generator;
+use Lavitto\FormToDatabase\Test\Functional\SiteBasedTestTrait;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Http\StreamFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Page\CacheHashCalculator;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+final class FormToDatabaseFinisherTest extends FunctionalTestCase
+{
+    use SiteBasedTestTrait;
+
+    final protected const LANGUAGE_PRESETS = [
+        'en' => [
+            'id' => 0,
+            'title' => 'TYPO3 Form Save To Database',
+            'locale' => 'en_GB',
+            'flag' => 'en',
+        ],
+    ];
+
+    protected array $testExtensionsToLoad = [
+        'lavitto/typo3-form-to-database',
+        'typo3conf/ext/form_to_database/Tests/Functional/Fixtures/test_extension',
+    ];
+
+    protected array $pathsToProvideInTestInstance = [
+        'typo3conf/ext/form_to_database/Tests/Functional/Finisher/Fixtures/fileadmin/' => 'fileadmin',
+    ];
+
+    protected array $coreExtensionsToLoad = [
+        'typo3/cms-form',
+        'typo3/cms-fluid-styled-content',
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/finisherBasicSetup.csv');
+
+        $site = $this->buildSiteConfiguration(
+            1,
+            'https://localhost/'
+        );
+
+        $site['dependencies'] = [
+            'lavitto/form-to-database-test',
+            'lavitto/typo3-form-to-database',
+            'typo3/fluid-styled-content',
+            'typo3/fluid-styled-content-css',
+        ];
+        $this->writeSiteConfiguration(
+            'test-base',
+            $site,
+            ['en' => $this->buildLanguageConfiguration('en', '/')]
+        );
+    }
+
+    /**
+     * @return Generator<string, array{
+     *     name: non-empty-string,
+     *     expectedResultFile: non-empty-string
+     * }>
+     */
+    public static function saveDoneDataProvider(): \Generator
+    {
+        yield 'Normal name saved correctly' => [
+            'name' => 'John Doe',
+            'expectedResultFile' => __DIR__ . '/Fixtures/Results/JohnDoeResult.csv',
+        ];
+
+        yield 'Name with umlauts saved correctly' => [
+            'name' => 'Ömer Üzgel',
+            'expectedResultFile' => __DIR__ . '/Fixtures/Results/UmlautResult.csv',
+        ];
+
+        yield 'Name with special characters saved correctly' => [
+            'name' => 'Tuğçe Çelik',
+            'expectedResultFile' => __DIR__ . '/Fixtures/Results/SpecialCharactersResult.csv',
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('saveDoneDataProvider')]
+    public function namesAreSavedCorrectToDatabase(string $name, string $expectedResultFile): void
+    {
+        $queryParams = [
+            'tx_form_formframework' => [
+                'action' => 'perform',
+                'controller' => 'FormFrontend',
+            ],
+            'id' => 1,
+        ];
+        $cacheHashCalculator = GeneralUtility::makeInstance(CacheHashCalculator::class);
+        $queryParams['cHash'] = $cacheHashCalculator->generateForParameters(http_build_query($queryParams));
+
+        unset($queryParams['id']);
+
+        $body = [
+            'tx_form_formframework' => [
+                'testform-1' => [
+                    '__state' => 'TzozOToiVFlQTzNcQ01TXEZvcm1cRG9tYWluXFJ1bnRpbWVcRm9ybVN0YXRlIjoyOntzOjI1OiIAKgBsYXN0RGlzcGxheWVkUGFnZUluZGV4IjtpOjA7czoxMzoiACoAZm9ybVZhbHVlcyI7YTowOnt9fQ==47a5df7cc91032d809f04ce767df252ca8bdcd67',
+                    '__session' => '8618e8dfe31948295237ebf3d69e5e6ff86a121e|5bd206863401e9ff977ea5b658bb4405b5ddd765',
+                    '__trustedProperties' => '{&quot;testform-1&quot;:{&quot;name&quot;:1,&quot;vR6Q2szYwtVrnZ81GqyN&quot;:1,&quot;__currentPage&quot;:1}}92cb9ee7e8aa1332939a703408f10973f2ab8307',
+                    '__currentPage' => 1,
+                    'vR6Q2szYwtVrnZ81GqyN' => '',
+                    'name' => $name,
+                ],
+            ],
+        ];
+
+        $streamFactory = GeneralUtility::makeInstance(StreamFactory::class);
+
+        $internalRequest = (new InternalRequest('https://localhost/'))
+            ->withQueryParams($queryParams)
+            ->withMethod('POST')
+            ->withBody($streamFactory->createStream(http_build_query($body)));
+
+        $result = $this->executeFrontendSubRequest($internalRequest);
+
+        $status = $result->getStatusCode();
+
+        self::assertEquals(200, $status);
+        self::assertCSVDataSet($expectedResultFile);
+    }
+}
---
+++ Tests/Functional/SiteBasedTestTrait.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lavitto\FormToDatabase\Test\Functional;
+
+/**
+ * Trait used for test classes that want to set up (= write) site configuration files.
+ *
+ * Mainly used when testing Site-related tests in Frontend requests.
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!NOTE!!! Be sure to set the LANGUAGE_PRESETS const in your class. !!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * @todo This trait has been copied from the TYPO3 core tests, because the `typo3/testing-framework` do not contain
+ *       this trait or similar feature set for now. This may change in the future, and this trait should then removed
+ *       along with adopting tests to the introduced TF way to deal with this.
+ */
+
+use TYPO3\CMS\Core\Configuration\SiteWriter;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Trait used for test classes that want to set up (= write) site configuration files.
+ *
+ * Mainly used when testing Site-related tests in Frontend requests.
+ *
+ * Be sure to set the LANGUAGE_PRESETS const in your class.
+ */
+trait SiteBasedTestTrait
+{
+    /**
+     * @param array{
+     *     rootPageId?: int,
+     *      base?: string
+     * } $site
+     * @param array<string, array{
+     *     languageId: int,
+     *     title: string,
+     *     navigationTitle: string,
+     *     websiteTitle: string,
+     *     base: string,
+     *     locale: string,
+     *     flag: string,
+     *     fallbackType: string
+     * }> $languages
+     * @param array<string, string> $errorHandling
+     */
+    protected function writeSiteConfiguration(
+        string $identifier,
+        array $site = [],
+        array $languages = [],
+        array $errorHandling = []
+    ): void {
+        $configuration = $site;
+        if (!empty($languages)) {
+            $configuration['languages'] = $languages;
+        }
+        if (!empty($errorHandling)) {
+            $configuration['errorHandling'] = $errorHandling;
+        }
+        $siteWriter = $this->get(SiteWriter::class);
+        try {
+            // ensure no previous site configuration influences the test
+            GeneralUtility::rmdir($this->instancePath . '/typo3conf/sites/' . $identifier, true);
+            $siteWriter->write($identifier, $configuration);
+        } catch (\Exception $exception) {
+            $this->markTestSkipped($exception->getMessage());
+        }
+    }
+
+    /**
+     * @return array{rootPageId: int, base: string}
+     */
+    protected function buildSiteConfiguration(
+        int $rootPageId,
+        string $base = ''
+    ): array {
+        return [
+            'rootPageId' => $rootPageId,
+            'base' => $base,
+        ];
+    }
+
+    /**
+     * @return array{
+     *       languageId: int,
+     *       title: string,
+     *       navigationTitle: string,
+     *       websiteTitle: string,
+     *       base: string,
+     *       locale: string,
+     *       flag: string|'global'
+     *   }
+     */
+    protected function buildDefaultLanguageConfiguration(
+        string $identifier,
+        string $base
+    ): array {
+        $configuration = $this->buildLanguageConfiguration($identifier, $base);
+        $configuration['flag'] = 'global';
+        unset($configuration['fallbackType'], $configuration['fallbacks']);
+        return $configuration;
+    }
+
+    /**
+     * @param array<array-key, mixed> $fallbackIdentifiers
+     * @return array{
+     *      languageId: int,
+     *      title: string,
+     *      navigationTitle: string,
+     *      websiteTitle: string,
+     *      base: string,
+     *      locale: string,
+     *      flag: string,
+     *      fallbackType: string,
+     *     fallbacks?: string
+     *  }
+     */
+    protected function buildLanguageConfiguration(
+        string $identifier,
+        string $base,
+        array $fallbackIdentifiers = [],
+        ?string $fallbackType = null
+    ): array {
+        $preset = $this->resolveLanguagePreset($identifier);
+
+        $configuration = [
+            'languageId' => $preset['id'],
+            'title' => $preset['title'],
+            'navigationTitle' => $preset['title'],
+            'websiteTitle' => $preset['websiteTitle'] ?? '',
+            'base' => $base,
+            'locale' => $preset['locale'],
+            'flag' => $preset['iso'] ?? '',
+            'fallbackType' => $fallbackType ?? (empty($fallbackIdentifiers) ? 'strict' : 'fallback'),
+        ];
+
+        if (!empty($fallbackIdentifiers)) {
+            $fallbackIds = array_map(
+                function (string $fallbackIdentifier) {
+                    $preset = $this->resolveLanguagePreset($fallbackIdentifier);
+                    return $preset['id'];
+                },
+                $fallbackIdentifiers
+            );
+            $configuration['fallbackType'] = $fallbackType ?? 'fallback';
+            $configuration['fallbacks'] = implode(',', $fallbackIds);
+        }
+
+        return $configuration;
+    }
+
+    protected function resolveLanguagePreset(string $identifier): mixed
+    {
+        if (!isset(static::LANGUAGE_PRESETS[$identifier])) {
+            throw new \LogicException(
+                sprintf('Undefined preset identifier "%s"', $identifier),
+                1533893665
+            );
+        }
+        return static::LANGUAGE_PRESETS[$identifier];
+    }
+}
