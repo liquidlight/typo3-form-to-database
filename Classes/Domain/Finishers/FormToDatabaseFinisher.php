1ef66af70ab4b61ace40c7431551101783ba2dab
Benjamin Serfhos <serfhos@rsm.nl>
2025-04-11 15:19:20 +0200

    [TASK] Fix phpstan messages

--- Classes/Domain/Finishers/FormToDatabaseFinisher.php
+++ Classes/Domain/Finishers/FormToDatabaseFinisher.php
@@ -149,8 +149,8 @@ class FormToDatabaseFinisher extends AbstractFinisher
 
         $formResult = GeneralUtility::makeInstance(FormResult::class);
         $formResult->setFormPersistenceIdentifier($formPersistenceIdentifier);
-        $formResult->setSiteIdentifier($request->getAttribute('site')->getIdentifier());
-        $formResult->setPid($request->getAttribute('frontend.page.information')->getId());
+        $formResult->setSiteIdentifier($request->getAttribute('site')?->getIdentifier() ?? '');
+        $formResult->setPid($request->getAttribute('frontend.page.information')?->getId() ?? 0);
         $formResult->setResultFromArray($formValues);
         $formResult->setFormPluginUid($formPluginUid);
         $formResult->setFormIdentifier($formIdentifier);
