28c9ef82fb3b1a246e92f8cb4a832c3d05e23561
Konrad Michalik <km@move-elevator.de>
2026-07-06 10:06:17 +0200

    fix: migrate finisher registration from deprecated TypoScript yamlConfigurations to auto-discovery

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,19 +6,4 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') or die();
-
-(static function (): void {
-    ExtensionManagementUtility::addTypoScriptSetup('
-        plugin.tx_form.settings.yamlConfigurations {
-          1560425499 = EXT:form_to_database/Configuration/Yaml/BaseSetup.yaml
-        }
-
-        module.tx_form.settings.yamlConfigurations {
-          1560425499 = EXT:form_to_database/Configuration/Yaml/BaseSetup.yaml
-          1560425500 = EXT:form_to_database/Configuration/Yaml/FormEditorSetup.yaml
-        }
-    ');
-})();
